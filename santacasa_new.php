d1d2c8525a5e45079d15c5a4e62a91051c7bdc73
santacasasjc <scalabrinih@gmail.com>
2021-11-26 13:24:49 -0300

    Erros Ok

--- santacasa_new.php
+++ santacasa_new.php
@@ -121,6 +121,100 @@
 <!--STATUS ULTIMOS 90 DIAS-->
 <!-------------------------->
 
+<?php 
+
+    if(@$_SESSION['cd_usu'] != ''){
+
+        $consulta_count_mes = "SELECT * FROM servicos";
+
+    }else{        
+
+        $consulta_count_mes = "SELECT * FROM servicos WHERE sn_ti <> 'S'";
+    }
+    
+    $result_count_mes = mysqli_query($conn, $consulta_count_mes);
+
+    while($row_count_mes = mysqli_fetch_array($result_count_mes)){
+
+        $cd_serv = $row_count_mes['cd_servico'];
+
+        echo '</br><div class="row justify-content-center">';
+            echo '<div class="col-md-11" style="border-style: solid; border-radius: 7px;border-width: thin; border-color: rgb(80, 9, 165);">';
+
+                echo '<div class="col-md-12" style="text-align: center !important;">
+                      <b style="font-size: 16px; color: #444444; text-align: left !important;">' . $row_count_mes['servico'] .'</b></br>'; 
+                    
+                    ////////////////////////
+                    //BARRAS STATUS DIARIO//
+                    //////////////////////// 
+                    
+                    $consulta_barras = "SELECT * FROM vw_status_diario WHERE cd_servico = $cd_serv";
+                    $result_barras = mysqli_query($conn, $consulta_barras);    
+
+                    while($row_barras = mysqli_fetch_array($result_barras)){
+
+                        if($row_barras['qtd_ocorrencias'] >= 1){
+
+    ?>  
+                            <!-------->
+                            <!--ERRO-->
+                            <!-------->                           
+
+                            <a href="#/" data-toggle="popover" data-html="true" style="text-decoration: none;" 
+                            title="<?php echo $row_barras['dia'] ?>" 
+                            data-content="
+                                <?php 
+
+                                    $consulta_erro = "SELECT * FROM vw_status_diario_detalhe 
+                                                      WHERE cd_servico = $cd_serv
+                                                      AND dia = '" . $row_barras['dia'] . "'";
+
+                                    $result_erro = mysqli_query($conn, $consulta_erro);    
+
+                                    while($row_erro = mysqli_fetch_array($result_erro)){                                                
+
+                                        echo 'Problema: '. $row_erro['ds_ocorrencia'];
+                                        echo '</br>Início: '. $row_erro['dt_inicio_conv'];
+
+                                        if(isset($row_erro['dt_fim_conv'])){
+
+                                            echo '</br>Fim: '. $row_erro['dt_fim_conv'];
+
+                                        }else{
+
+                                            echo '</br>Fim: Em correção';
+                                        }
+                                        
+
+
+                                    }
+                                ?>
+                            ">
+                            <img style="margin-top: 5px; margin-bottom: 5px;" src="img/barra_erro.png" height="25px" width="5px" class="d-inline-block align-top" > </a>
+
+<?php
+                        }     
+                    }             
+                   
+                    ////////////////////////
+                    //ESTABILIDADE SERVICO//
+                    ////////////////////////
+
+                    $consulta_estabilidade = "SELECT * FROM vw_estabilidade_total WHERE cd_servico = $cd_serv";
+                    $result_estabilidade = mysqli_query($conn, $consulta_estabilidade);
+                    $row_estabilidade = mysqli_fetch_array($result_estabilidade);
+
+                    echo '<center>' . $row_estabilidade['porc_estab'] . '% de estabilidade (Últimos 90 dias) </center>';
+?>
+                
+                </div>
+            
+            </div>
+
+        </div>       
+
+    <?php } ?>
+
 <?php
 
     include "rodape_oco.php";
